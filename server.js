1a73b8b84a584c70999eb8827742f79b1b7e1790
Sankeerthana04 <sankeerthanajannu8@gmail.com>
2024-09-28 14:31:52 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,6 @@
-const express=require('express')
-const MongoClient=require('mongoose')
-const dotEnv=require('dotenv')
-const bodyParser=require('body-parser')
-const EmployeeRoute=require('./routes/EmployeeRoute')
+const express=require('express');
 const app=express();
-app.use(bodyParser.json());
-dotEnv.config();
 
-const PORT=process.env.PORT || 7766;
-MongoClient.connect(process.env.MONGO_URI)
-.then(()=>{
-    console.log("MongoDB connection is success")
-})
-.catch((error)=>{
-    console.log(error);
-})
-
-app.use("/employees",EmployeeRoute);
-
-app.listen(PORT,()=>{
-    console.log('My Server is running on ${PORT} number')
+app.listen(5500,()=>{
+    console.log("My server is on port 5500");
 })
